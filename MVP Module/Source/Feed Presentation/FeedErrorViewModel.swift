7c88d299a4b0a913e7eec61073a886faa72fe762
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-05-28 18:32:01 +0300

    Localize all screens

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedErrorViewModel.swift
+//  FeedFeature
+//
+//  Created by Ali Emre Değirmenci on 28.05.2020.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String
+}
--- MVP Module/Source/Feed UI/Composers/WeakRefVirtualProxy.swift
+++ MVP Module/Source/Feed UI/Composers/WeakRefVirtualProxy.swift
@@ -5,21 +5,27 @@
 import UIKit
 
 final class WeakRefVirtualProxy<T: AnyObject> {
-	private weak var object: T?
-	
-	init(_ object: T) {
-		self.object = object
-	}
+    private weak var object: T?
+
+    init(_ object: T) {
+        self.object = object
+    }
 }
 
 extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
-	func display(_ viewModel: FeedLoadingViewModel) {
-		object?.display(viewModel)
-	}
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: FeedErrorView where T: FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel) {
+        object?.display(viewModel)
+    }
 }
 
 extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
-	func display(_ model: FeedImageViewModel<UIImage>) {
-		object?.display(model)
-	}
+    func display(_ model: FeedImageViewModel<UIImage>) {
+        object?.display(model)
+    }
 }
